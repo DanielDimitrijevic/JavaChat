7bf855bfd726b81faefeb0dd7bdc8aa9fa789312
Dominik Backhausen <Dominik.Backhausen@gmail.com>
2013-03-19 20:32:28 +0100

    Lösche unnötige files

--- src/Server/Server_ConectionReader.java
+++ src/Server/Server_ConectionReader.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import Server_Test.ConectedClient;
 
 public class Server_ConectionReader implements Runnable{
 	private Server_Controller sc;
--- src/Server_Test/JChatServer.java
+++
@@ -1,8 +0,0 @@
-package Server_Test;
-
-public class JChatServer {
-	public static void main(String[] args){
-		ServerConection server = null;
-	         server = new ServerConection(2233);
-	}
-}
